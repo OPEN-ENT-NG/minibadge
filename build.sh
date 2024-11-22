8eb8bfafcfd43cf5ae3ab074f7c8eebb1bb30f05
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-11-22 17:10:18 +0100

    chore: remove maven.test.skip=true

--- build.sh
+++ build.sh
@@ -81,7 +81,7 @@ publish() {
             ;;
     esac
 
-    docker-compose run --rm maven mvn -DrepositoryId=ode-$nexusRepository -DskiptTests -Dmaven.test.skip=true --settings /var/maven/.m2/settings.xml deploy
+    docker-compose run --rm maven mvn -DrepositoryId=ode-$nexusRepository -DskiptTests --settings /var/maven/.m2/settings.xml deploy
 }
 
 publishNexus() {
@@ -91,7 +91,7 @@ publishNexus() {
     *SNAPSHOT) export nexusRepository='snapshots' ;;
     *)         export nexusRepository='releases' ;;
   esac
-  docker compose run --rm  maven mvn -DrepositoryId=ode-$nexusRepository -Durl=$repo -DskipTests -Dmaven.test.skip=true --settings /var/maven/.m2/settings.xml deploy
+  docker compose run --rm  maven mvn -DrepositoryId=ode-$nexusRepository -Durl=$repo -DskipTests --settings /var/maven/.m2/settings.xml deploy
 }
 
 init() {
