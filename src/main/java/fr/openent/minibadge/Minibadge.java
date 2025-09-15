4cd0440b2b39cdb273cbdc3b26245701d0eb62de
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-09-15 16:23:26 +0200

    feat: #RBACK-117, kubernetes compatibility

--- src/main/java/fr/openent/minibadge/Minibadge.java
+++ src/main/java/fr/openent/minibadge/Minibadge.java
@@ -3,6 +3,7 @@ package fr.openent.minibadge;
 import fr.openent.minibadge.controller.*;
 import fr.openent.minibadge.model.Config;
 import fr.openent.minibadge.service.ServiceRegistry;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import org.entcore.common.http.BaseServer;
 
@@ -16,12 +17,16 @@ public class Minibadge extends BaseServer {
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
-        ServiceRegistry.initServices();
-
-        super.start(startPromise);
-
-        dbSchema = config.getString("db-schema", MINIBADGE);
-        minibadgeConfig = new Config(config);
+      ServiceRegistry.initServices();
+      final Promise<Void> promise = Promise.promise();
+      super.start(promise);
+      promise.future()
+        .compose(e -> this.initMinibadge())
+        .onComplete(startPromise);
+    }
+    public Future<Void> initMinibadge() {
+		dbSchema = config.getString("db-schema");
+	    minibadgeConfig = new Config(config);
 
         addController(new MinibadgeController());
         addController(new SettingController());
@@ -33,6 +38,6 @@ public class Minibadge extends BaseServer {
         addController(new UserController());
         addController(new ConfigController());
 
-        startPromise.tryComplete();
+	    return Future.succeededFuture();
     }
 }
