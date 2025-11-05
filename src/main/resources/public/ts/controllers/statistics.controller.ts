4fb21f2746f464578c280958c052113a3af66182
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2025-11-05 10:14:18 +0100

    wip front

--- src/main/resources/public/ts/controllers/statistics.controller.ts
+++ src/main/resources/public/ts/controllers/statistics.controller.ts
@@ -1,6 +1,7 @@
 import { IScope } from "angular";
 import { model, moment, ng, notify } from 'entcore';
 import { rights } from '../core/constants/rights.const';
+import { StatsPeriod } from "../core/enum/stats-period.enum";
 import { Setting } from "../models/setting.model";
 import { Statistics } from "../models/statistic.model";
 import { IStatisticService } from "../services";
@@ -9,8 +10,9 @@ import { safeApply } from "../utils/safe-apply.utils";
 import { translate } from "../utils/string.utils";
 
 interface ViewModel {
-    startDate: Date;
+    startDate: Date | null;
     endDate: Date;
+    selectedPeriod: StatsPeriod;
     translate: typeof translate;
     toLocaleString: typeof toLocaleString;
     statistics: Statistics;
@@ -24,21 +26,52 @@ interface IMinibadgeScope extends IScope {
 }
 
 class Controller implements ng.IController, ViewModel {
-    startDate: Date;
+    startDate: Date | null;
     endDate: Date;
     translate: typeof translate = translate;
     toLocaleString: typeof toLocaleString = toLocaleString;
     statistics: Statistics;
+    selectedPeriod: StatsPeriod;
 
     constructor(private $scope: IMinibadgeScope, private statisticService: IStatisticService) {
         this.endDate = new Date();
-        this.startDate = moment().subtract('months', 1).toDate();
+        this.startDate = null;
         this.$scope.vm = this;
         this.statistics = new Statistics();
+        this.selectedPeriod = StatsPeriod.ALL;
+    }
+
+    onChangePeriod(): void {
+        this.updateStartDate();
+        this.statisticService.getStatistics(this.startDate)
+            .then((data: Statistics) => {
+                if (data) this.statistics = data;
+                safeApply(this.$scope);
+            })
+            .catch(() => notify.error('minibadge.error.get.statistics'));
+        safeApply(this.$scope);
+    }
+
+    private updateStartDate(): void {
+        const today = moment();
+
+        switch (this.selectedPeriod) {
+            case StatsPeriod.ALL:
+                this.startDate = null;
+                break;
+            case StatsPeriod.LAST_30_DAYS:
+                this.startDate = today.clone().subtract(30, 'days').toDate();
+                break;
+            case StatsPeriod.LAST_7_DAYS:
+                this.startDate = today.clone().subtract(7, 'days').toDate();
+                break;
+            default:
+                this.startDate = null;
+        }
     }
 
     $onInit() {
-        this.statisticService.getStatistics()
+        this.statisticService.getStatistics(null)
             .then((data: Statistics) => {
                 if (data) this.statistics = data;
                 safeApply(this.$scope);
---
+++ src/main/resources/public/ts/core/enum/stats-period.enum.ts
@@ -0,0 +1,5 @@
+export enum StatsPeriod {
+    ALL = 'all',
+    LAST_30_DAYS = '30days',
+    LAST_7_DAYS = '7days'
+}
